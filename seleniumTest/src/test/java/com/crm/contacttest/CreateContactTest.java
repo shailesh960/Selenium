ab794786ea5457b15f06ec43cab78736c64bee34
om <om@DESKTOP-HSN9QIK>
2021-12-30 15:40:53 -0500

    My First commit of the selenium Repo

---
+++ seleniumTest/src/test/java/com/crm/contacttest/CreateContactTest.java
@@ -0,0 +1,20 @@
+package com.crm.contacttest;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest 
+{
+	@Test
+	public void creatContact()
+	{
+		System.out.println("CContact is created");
+	}
+	
+	@Test
+	public void updateContact()
+	{
+		System.out.println("Contact is Updated");
+	}
+	
+
+}
---
+++ seleniumTest/src/test/java/com/crm/organization/CreateOrganizationTest.java
@@ -0,0 +1,21 @@
+package com.crm.organization;
+
+import org.testng.annotations.Test;
+
+public class CreateOrganizationTest
+{
+	@Test
+	public void  createOrganization()
+	{
+		System.out.println("Organization is created");
+	}
+	
+	@Test
+	public void updateOrganization()
+	{
+		System.out.println("Ogranization is updated");
+	}
+	
+	
+
+}
